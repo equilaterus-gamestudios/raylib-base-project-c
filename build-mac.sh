eea51762c6c9614b13fd58cb856144ca2cabe3ca
dacanizares <dacanizares@outlook.com>
2025-04-28 10:22:11 -0500

    Fixed build mac
    Co-Authored-By: Edwin Osorio <30908546+edwin1106@users.noreply.github.com>
    Co-Authored-By: Juan Felipe Cañizares Corrales <pipecaniza@outlook.com>

--- build-mac.sh
+++ build-mac.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 mkdir -p bin/mac
-cp -r out/. bin/linux/
-gcc src/main.c -g -ggdb -w -lraylib -lm -o bin/mac/main -std=c99 -pedantic
+cp -r root/. bin/mac/
+gcc src/main.c -g -ggdb -w -lraylib -lm -o bin/mac/run -std=c99 -pedantic
